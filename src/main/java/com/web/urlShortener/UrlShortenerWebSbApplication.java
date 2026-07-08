603457cdb7e6f45c0d7294ca5fef8e523c4010fa
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-07-08 12:00:26 +0530

    first commit

---
+++ src/main/java/com/web/urlShortener/UrlShortenerWebSbApplication.java
@@ -0,0 +1,13 @@
+package com.web.urlShortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerWebSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerWebSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/urlShortener/UrlShortenerWebSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.urlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerWebSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
