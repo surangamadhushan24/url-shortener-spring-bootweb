334d1e5812bf55099af0ee96e27a336a86cca273
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-07-08 21:58:29 +0530

    create home controller

---
+++ src/main/java/com/web/urlShortener/HomeController.java
@@ -0,0 +1,26 @@
+package com.web.urlShortener;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title", "URl -Shortner");
+		return "index";
+	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about";
+	}
+	
+	
+
+}
