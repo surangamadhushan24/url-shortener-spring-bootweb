445f1146ff9073e5c3cc38e21b35c77e616a4f2b
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-08-01 11:42:16 +0530

    update UserDto

--- src/main/java/com/web/urlShortener/domain/dtos/UserDto.java
+++ src/main/java/com/web/urlShortener/domain/dtos/UserDto.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public record UserDto(
 		Long id,
-		String email,
 		String name) implements Serializable {
 
 }
--- src/main/java/com/web/urlShortener/domain/mappers/UserMapper.java
+++ src/main/java/com/web/urlShortener/domain/mappers/UserMapper.java
@@ -2,10 +2,15 @@ package com.web.urlShortener.domain.mappers;
 
 import org.mapstruct.Mapper;
 
+import com.web.urlShortener.domain.dtos.UserDto;
+import com.web.urlShortener.domain.entities.User;
+
 
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+	
+	UserDto toDto(User user);
 	
 	
 	
