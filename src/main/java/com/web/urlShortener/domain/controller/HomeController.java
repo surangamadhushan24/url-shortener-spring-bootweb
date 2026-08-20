539c68ec6bef2c4886bf7f34e16416cc8f394157
suranga madhushan <150942338+surangamadhushan24@users.noreply.github.com>
2026-08-20 22:33:56 +0530

    feat:implement post mapping for form

--- src/main/java/com/web/urlShortener/domain/controller/HomeController.java
+++ src/main/java/com/web/urlShortener/domain/controller/HomeController.java
@@ -4,31 +4,57 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.web.urlShortener.domain.dtos.CreateShortUrlForm;
 import com.web.urlShortener.domain.dtos.ShortUrlDto;
-import com.web.urlShortener.domain.entities.ShortUrl;
+
 import com.web.urlShortener.domain.services.ShortUrlService;
 
-
-
+import jakarta.validation.Valid;
 
 @Controller
 public class HomeController {
-	
+
 	private final ShortUrlService shortUrlService;
 
-    public HomeController(ShortUrlService shortUrlService) {
-        this.shortUrlService = shortUrlService;
-    }
+	public HomeController(ShortUrlService shortUrlService) {
+		this.shortUrlService = shortUrlService;
+	}
 
-    @GetMapping("/")
-    public String home(Model model) {
-        List<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls();
-        model.addAttribute("shortUrls", shortUrls);
-        model.addAttribute("baseUrl", "http://localhost:8080");
-        return "index";
-    }
+	@GetMapping("/")
+	public String home(Model model) {
+		List<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls();
+		model.addAttribute("shortUrls", shortUrls);
+		model.addAttribute("baseUrl", "http://localhost:8080");
+		model.addAttribute("createShortUrlForm", new CreateShortUrlForm(""));
+		return "index";
+	}
+
+	@PostMapping("/short-urls")
+    public String createShortUrl(@ModelAttribute("createShortUrlForm") @Valid CreateShortUrlForm form,
+    		BindingResult bindingResult,
+    		RedirectAttributes redirectAttributes,
+    		Model model) {
+    	
+    	if(bindingResult.hasErrors()) {
+    		 List<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls();
+    		 model.addAttribute("shortUrls", shortUrls);
+		     model.addAttribute("baseUrl", "http://localhost:8080");     
+		     return "index";
+    		 
+		   //Todo 
+		     
+		     redirectAttributes.addFlashAttribute("successMessage", "Short URL created successfully!");
+		     return "redirect:/";
+		     
+	}
+    
+    
 	
 	
 	
